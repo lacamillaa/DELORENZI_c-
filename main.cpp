aa35e1afd567d4e491257fb18c07636d3ba64c57
lacamillaa <12999356@itisrossi.vi.it>
2026-01-28 10:18:20 +0100

    metodi per la creazione di array

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
+#include <array>
 #include <cstring>
 #include <iostream>
+#include "Persona.h"
 
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    string nome;
+    /*string nome;
     cout << "Inserisci il nome: ";
     // cin >> nome; // legge fino al primo spazio bianco
     getline(cin, nome); // legge tutto il buffer
@@ -14,6 +15,18 @@ int main() {
     persona->stampaDati();
     delete persona;
     // con delete libera la memoria e invoca il distruttore
-    persona = nullptr;
+    persona = nullptr;*/
+
+    int arr[5]; // array STATICO, va nello stack
+    int arr2[10] = {1, 2, 4, 8, 16, 32, 64, 128}; // array STATICO
+    arr[1] = 2;
+
+    int* arrd = new int[10]; // array DINAMICO, va nello heap
+    arrd[0] = 3;
+    delete[] arrd; // libera lo spazio dedicato all'array
+
+    array<int, 7> arr3 = {1, 2, 3, 4, 5, 6};
+    cout << arr3[0] << endl;
+    cout << arr3.size() << endl; // 7
     return 0;
 }
